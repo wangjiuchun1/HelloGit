5fd132e78abcf767385f52235d4255a8dc72f1f6
wangjiuchun1 <862944586@qq.com>
2021-11-21 17:05:02 +0800

    Hello Git

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2021-11-21 16:48
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+
+    }
+}
